acf229827f0ab221e6aa683f4ef128fe82747419
Pascal <pascal@Pascals-MacBook-Pro.local>
2022-07-16 02:45:42 +0200

    Create device functionalities

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
 
+ # root to: "home#index"
   root to: "groups#index"
   resources :groups, only: [:index,:new, :create] do
     resources :entities, only: [:index]
